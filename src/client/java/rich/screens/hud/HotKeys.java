b088d2c6f992017d293b89c594d86245e0d44e4d
mishkaposral221-boop <mishkaposral221@gmail.com>
2026-06-07 12:36:39 +0400

    Use vanilla text renderer for HotKeys HUD

--- src/client/java/rich/screens/hud/HotKeys.java
+++ src/client/java/rich/screens/hud/HotKeys.java
@@ -11,7 +11,6 @@ import rich.modules.module.ModuleStructure;
 import rich.theme.ClientTheme;
 import rich.util.animations.Direction;
 import rich.util.render.Render2D;
-import rich.util.render.font.Fonts;
 import rich.util.string.KeyHelper;
 
 public class HotKeys extends AbstractHudElement {
@@ -22,6 +21,7 @@ public class HotKeys extends AbstractHudElement {
    private float animatedHeight = 23.0F;
    private long lastUpdateTime = System.currentTimeMillis();
    private static final float ANIMATION_SPEED = 8.0F;
+   private static final float TEXT_SCALE = 0.75F;
 
    public HotKeys() {
       super("HotKeys", 300, 40, 80, 23, true);
@@ -69,6 +69,18 @@ public class HotKeys extends AbstractHudElement {
       return var1 + (var2 - var1) * var4;
    }
 
+   private float textWidth(String var1) {
+      return this.mc.textRenderer.getWidth(var1) * TEXT_SCALE;
+   }
+
+   private void drawHudText(DrawContext var1, String var2, float var3, float var4, int var5) {
+      var1.getMatrices().pushMatrix();
+      var1.getMatrices().translate(var3, var4);
+      var1.getMatrices().scale(TEXT_SCALE, TEXT_SCALE);
+      var1.drawText(this.mc.textRenderer, var2, 0, 0, var5, false);
+      var1.getMatrices().popMatrix();
+   }
+
    @Override
    public void drawDraggable(DrawContext var1, int var2) {
       if (var2 > 0) {
@@ -87,15 +99,15 @@ public class HotKeys extends AbstractHudElement {
             var11 += 11;
             String var13 = "Example Module";
             String var14 = "[" + this.currentRandomKey + "]";
-            float var15 = Fonts.BOLD.getWidth(var14, 6.0F);
-            float var16 = Fonts.BOLD.getWidth(var13, 6.0F);
+            float var15 = this.textWidth(var14);
+            float var16 = this.textWidth(var13);
             var12 = Math.max(var16 + var15 + 50.0F, var12);
          } else {
             for (ModuleStructure var33 : this.keysList) {
                var11 += 11;
                String var35 = "[" + KeyHelper.getKeyName(var33.getKey()) + "]";
-               float var37 = Fonts.BOLD.getWidth(var35, 6.0F);
-               float var17 = Fonts.BOLD.getWidth(var33.getName(), 6.0F);
+               float var37 = this.textWidth(var35);
+               float var17 = this.textWidth(var33.getName());
                var12 = Math.max(var17 + var37 + 50.0F, var12);
             }
          }
@@ -122,26 +134,26 @@ public class HotKeys extends AbstractHudElement {
 
          long var38 = this.keysList.size();
          String var18 = String.valueOf(var38);
-         float var19 = Fonts.BOLD.getWidth(var18, 6.0F);
-         float var20 = Fonts.BOLD.getWidth("Active:", 6.0F);
+         float var19 = this.textWidth(var18);
+         float var20 = this.textWidth("Active:");
          Render2D.gradientRect(var7 + this.getWidth() - var19 - var20 + 2.0F, var8 + 5.0F, 14.0F, 12.0F, ClientTheme.panelGradient(var36), 3.0F);
-         Fonts.HUD_ICONS.draw("g", var7 + this.getWidth() - var19 - var20 + 4.0F, var8 + 6.0F, 10.0F, new Color(165, 165, 165, var36).getRGB());
-         Fonts.BOLD.draw("Binds", var7 + 8.0F, var8 + 6.5F, 6.0F, new Color(255, 255, 255, var36).getRGB());
+         this.drawHudText(var1, var18, var7 + this.getWidth() - var19 - var20 + 5.0F, var8 + 7.0F, new Color(165, 165, 165, var36).getRGB());
+         this.drawHudText(var1, "Binds", var7 + 8.0F, var8 + 6.5F, new Color(255, 255, 255, var36).getRGB());
          byte var21 = 23;
          if (var10) {
             String var22 = "Example Module";
             String var23 = "[" + this.currentRandomKey + "]";
-            float var24 = Fonts.BOLD.getWidth(var23, 6.0F);
+            float var24 = this.textWidth(var23);
             float var25 = var7 + this.getWidth() - var24 - 11.5F;
             Render2D.gradientRect(var25, var8 + var21 - 2.0F, var24 + 4.0F, 9.0F, ClientTheme.panelGradient(var36), 3.0F);
             Render2D.outline(var25, var8 + var21 - 2.0F, var24 + 4.0F, 9.0F, 0.05F, ClientTheme.outline(var36), 2.0F);
             Render2D.rect(var7 + 8.0F, var8 + var21 - 1.0F, 1.0F, 7.0F, ClientTheme.textSub((int)(128.0F * var3)), 1.0F);
-            Fonts.BOLD.draw(var22, var7 + 13.0F, var8 + var21 - 1.5F, 6.0F, ClientTheme.text(var36));
-            Fonts.BOLD.draw(var23, var25 + 2.0F, var8 + var21 - 1.0F, 6.0F, ClientTheme.textSub(var36));
+            this.drawHudText(var1, var22, var7 + 13.0F, var8 + var21 - 1.5F, ClientTheme.text(var36));
+            this.drawHudText(var1, var23, var25 + 2.0F, var8 + var21 - 1.0F, ClientTheme.textSub(var36));
          } else {
             for (ModuleStructure var40 : this.keysList) {
                String var41 = "[" + KeyHelper.getKeyName(var40.getKey()) + "]";
-               float var42 = Fonts.BOLD.getWidth(var41, 6.0F);
+               float var42 = this.textWidth(var41);
                int var26 = ClientTheme.text(var36);
                int var27 = ClientTheme.textSub(var36);
                int var28 = ClientTheme.textSub((int)(128.0F * var3));
@@ -149,8 +161,8 @@ public class HotKeys extends AbstractHudElement {
                Render2D.gradientRect(var29, var8 + var21 - 2.0F, var42 + 4.0F, 9.0F, ClientTheme.panelGradient(var36), 3.0F);
                Render2D.outline(var29, var8 + var21 - 2.0F, var42 + 4.0F, 9.0F, 0.05F, ClientTheme.outline(var36), 2.0F);
                Render2D.rect(var7 + 8.0F, var8 + var21 - 1.0F, 1.0F, 7.0F, var28, 1.0F);
-               Fonts.BOLD.draw(var40.getName(), var7 + 13.0F, var8 + var21 - 1.5F, 6.0F, var26);
-               Fonts.BOLD.draw(var41, var29 + 2.0F, var8 + var21 - 1.0F, 6.0F, var27);
+               this.drawHudText(var1, var40.getName(), var7 + 13.0F, var8 + var21 - 1.5F, var26);
+               this.drawHudText(var1, var41, var29 + 2.0F, var8 + var21 - 1.0F, var27);
                var21 += 11;
             }
          }
